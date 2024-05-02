dd8622418651e4ef1caf48fab61b4c13fa9ef6fe
Sir-ko <ibugvaev20000@gmail.com>
2024-05-02 23:28:28 +0500

    КАМЕРА РАБОТАЕТ ПОЛНОЦЕННО!!!!!!!!!!!!!!!

--- TheGame_SaintSouls/Assets/Prefabs/Technical Staff/For CameraGBC/QuadScale.cs
+++ TheGame_SaintSouls/Assets/Prefabs/Technical Staff/For CameraGBC/QuadScale.cs
@@ -2,12 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class QuadScale : MonoBehaviour
 {
     public Camera Camera;
     void Start()
     {
-        var quadHeight = Camera.main.orthographicSize * 2.0;
+        var quadHeight = Camera.orthographicSize * 2.0;
         var quadWidth = quadHeight * Screen.width / Screen.height;
         transform.localScale = new Vector3((float)quadWidth, (float)quadHeight, 1f);
     }
