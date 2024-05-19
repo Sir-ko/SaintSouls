0997a754f355e3c94984e047cefb580983c2c3bc
mistakeo <4mistakeo@gmail.com>
2024-05-19 18:15:08 +0500

    патрулирование монстра (fixed)

--- TheGame_SaintSouls/Assets/SCRIPTS/EnemyAI.cs
+++ TheGame_SaintSouls/Assets/SCRIPTS/EnemyAI.cs
@@ -35,7 +35,7 @@ public class EnemyAI : MonoBehaviour
     {
         if (!_isPlayerNoticed)
         {
-            if (_navMeshAgent.remainingDistance == 0)
+            if (_navMeshAgent.remainingDistance <= _navMeshAgent.stoppingDistance)
             {
                 PickNewPatrolPoint();
             }
